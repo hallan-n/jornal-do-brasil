06b87a2d8e6af2f9c2ac5f19e44f717205fe4b13
hallan-n <hallanneves01@gmail.com>
2023-05-18 20:45:25 -0300

    Add crud on controller

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,6 +1,24 @@
 package controller;
-public class UsuarioController {
-    public UsuarioController(){
-        
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioController{
+
+    public boolean create(Object object) {
+        return true;
     }
+
+    public List<Object> read() {
+        return new ArrayList();
+    }
+
+    public boolean update(Object object) {
+        return true;
+    }
+
+    public boolean delete(int id) {
+        return true;
+    }
+
 }
