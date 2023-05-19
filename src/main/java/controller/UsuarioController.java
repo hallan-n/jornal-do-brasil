4431d94047ede6540202e0fdca870447183a4949
hallan-n <hallanneves01@gmail.com>
2023-05-18 21:11:42 -0300

    Add servlet

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,24 +1,76 @@
 package controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class UsuarioController{
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-    public boolean create(Object object) {
-        return true;
+import model.Usuario;
+public class UsuarioController extends HttpServlet {
+
+    // Variaveis globais
+    private String acao, abrir;
+    private final String cadastrar = "cadastrar_aluno.jsp";
+    private final String editar = "cadastrar_aluno.jsp";
+    private final String listar = "listar_alunos.jsp";
+    private final String sucesso = "sucesso.jsp";
+    private final String erro = "erro.jsp";
+    private final static String a = "asd";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        }
     }
 
-    public List<Object> read() {
-        return new ArrayList();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+     
+        acao = request.getParameter("acao");
+        
+        if (acao.equals("listar")) {
+            
+            AlunoDAO dao = new AlunoDAO();
+            abrir = listar;
+            request.setAttribute("alunos", dao.read());
+            
+        }
+        
+        RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
+        visualizar.forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        acao = request.getParameter("acao");
+        if (acao.equals("cadastrar")) {
+            Aluno aluno = new Aluno();
+            aluno.setNome(request.getParameter("txtNome"));
+            aluno.setEmail(request.getParameter("txtEmail"));
+            aluno.setIdade(Integer.parseInt(request.getParameter("txtIdade")));
+            aluno.setTelefone(request.getParameter("txtTelefone"));
+            AlunoDAO dao = new AlunoDAO();
+            if (dao.create(aluno)) {
+                abrir = sucesso;
+                request.setAttribute("msg", "Uhull... Aluno cadastrado com sucesso!");
+            } else {
+                abrir = erro;
+                request.setAttribute("msg", "Ops... Erro ao cadastrar Aluno!");
+            }
+        }
+        RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
+        visualizar.forward(request, response);
     }
 
-    public boolean update(Object object) {
-        return true;
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
-
-    public boolean delete(int id) {
-        return true;
-    }
-
 }
---
+++ src/main/java/controller/_UsuarioController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class _UsuarioController{
+
+    public boolean create(Object object) {
+        return true;
+    }
+
+    public List<Object> read() {
+        return new ArrayList();
+    }
+
+    public boolean update(Object object) {
+        return true;
+    }
+
+    public boolean delete(int id) {
+        return true;
+    }
+
+}
