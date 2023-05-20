6eb0e6cc04f7707178cfc762f0615ba627a059e0
hallan-n <hallanneves01@gmail.com>
2023-05-20 02:20:24 -0300

    Edditing query of database

--- db_jornal_do_brasil.sql
+++ db_jornal_do_brasil.sql
@@ -3,30 +3,28 @@ use db_jornal_do_brasil;
 
 
 CREATE TABLE address (
-	idAddress INT NOT NULL AUTO_INCREMENT,
+	idAddress INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	number VARCHAR(9999) NOT NULL,
 	complement VARCHAR(45) NOT NULL,
 	district VARCHAR(45) NOT NULL,
 	city VARCHAR(45) NOT NULL,
 	state VARCHAR(45) NOT NULL,
-	zipCode VARCHAR(9) NOT NULL,
-	PRIMARY KEY (idAddress)
+	zipCode VARCHAR(9) NOT NULL
 );
 
 CREATE TABLE configuration (
-	idConfig INT NOT NULL,
+	idConfig INT PRIMARY KEY  NOT NULL,
 	notification BOOL NOT NULL,
 	sound BOOL NOT NULL,
 	showProfilePhoto BOOL NOT NULL,
 	showdescription BOOL NOT NULL,
 	showNumber BOOL NOT NULL,
 	showEmail BOOL NOT NULL,
-	theme ENUM("Dark","White") NOT NULL,
-	PRIMARY KEY (idConfig)
+	theme ENUM("Dark","White") NOT NULL
 );
 
 CREATE TABLE user (
-	idUser INT NOT NULL AUTO_INCREMENT,
+	idUser INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	name VARCHAR(45) NOT NULL,
 	surname VARCHAR(45) NOT NULL,
 	email VARCHAR(45) NOT NULL,
@@ -39,19 +37,17 @@ CREATE TABLE user (
 	configuration INT NOT NULL,
 
 	FOREIGN KEY (configuration) REFERENCES configuration(idConfig),
-	FOREIGN KEY (address) REFERENCES address(idAddress),
-	PRIMARY KEY (idUser)
+	FOREIGN KEY (address) REFERENCES address(idAddress)
 );
 
 CREATE TABLE publication(
-	idPubli INT NOT NULL AUTO_INCREMENT,
+	idPubli INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	title VARCHAR(45) NOT NULL,
 	category ENUM("Política","Business","Internacional","Esportes","Saúde","Tecnologia","Entretenimento","Estilo","Gastronomia") NOT NULL,
 	path VARCHAR(45) NOT NULL,
 	author INT NOT NULL,
 	date DATE NOT NULL,
 
-	FOREIGN KEY (author) REFERENCES user(idUser),
-	PRIMARY KEY (idPubli)
+	FOREIGN KEY (author) REFERENCES user(idUser)
 );
  
