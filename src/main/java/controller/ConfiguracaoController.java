778d2b86a638f9fee5aa36001960b432be983637
hallan-n <hallanneves01@gmail.com>
2023-05-23 08:37:28 -0300

    Revert "Finish CRUD in COnfiguration.java"
    This reverts commit 3fc1053698e4f509f0b9a13a71d4cdfb686ca03c.

--- src/main/java/controller/ConfiguracaoController.java
+++ src/main/java/controller/ConfiguracaoController.java
@@ -1,31 +1,9 @@
 package controller;
-
 import dao.ConfiguracaoDAO;
 import model.Configuration;
 
 public class ConfiguracaoController {
-    public static void main(String[] args) {
-
-        ConfiguracaoDAO configuracaoDAO = new ConfiguracaoDAO();
-
-        // Configuration configuration = new Configuration(true,true,true,true,true,true,"Dark");
-        // configuracaoDAO.create(configuration);
-        
-        // Configuration configuration = new Configuration();
-        // configuration.setIdConfig(2);
-        // configuration.setNotification(false);
-        // configuration.setSound(false);
-        // configuration.setShowProfilePhoto(false);
-        // configuration.setShowdescription(false);
-        // configuration.setShowNumber(false);
-        // configuration.setShowEmail(false);
-        // configuration.setTheme("Dark");
-        // configuracaoDAO.update(configuration);
-        
-        // configuracaoDAO.deleteForId(4);
-        
-        // for (Configuration i : configuracaoDAO.read()) {
-        //     System.out.print(i.getIdConfig()+" | "+i.isNotification()+" | "+i.isSound()+" | "+i.isShowProfilePhoto()+" | "+i.isShowdescription()+" | "+i.isShowNumber()+" | "+i.isShowEmail()+" | "+i.getTheme()+"\n");
-        // }
+    public void crud(){
+        ConfiguracaoDAO configDAO = new ConfiguracaoDAO();
     }
 }
