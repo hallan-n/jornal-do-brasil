3fc1053698e4f509f0b9a13a71d4cdfb686ca03c
hallan-n <hallanneves01@gmail.com>
2023-05-22 21:35:08 -0300

    Finish CRUD in COnfiguration.java

--- src/main/java/controller/ConfiguracaoController.java
+++ src/main/java/controller/ConfiguracaoController.java
@@ -1,9 +1,31 @@
 package controller;
+
 import dao.ConfiguracaoDAO;
 import model.Configuration;
 
 public class ConfiguracaoController {
-    public void crud(){
-        ConfiguracaoDAO configDAO = new ConfiguracaoDAO();
+    public static void main(String[] args) {
+
+        ConfiguracaoDAO configuracaoDAO = new ConfiguracaoDAO();
+
+        // Configuration configuration = new Configuration(true,true,true,true,true,true,"Dark");
+        // configuracaoDAO.create(configuration);
+        
+        // Configuration configuration = new Configuration();
+        // configuration.setIdConfig(2);
+        // configuration.setNotification(false);
+        // configuration.setSound(false);
+        // configuration.setShowProfilePhoto(false);
+        // configuration.setShowdescription(false);
+        // configuration.setShowNumber(false);
+        // configuration.setShowEmail(false);
+        // configuration.setTheme("Dark");
+        // configuracaoDAO.update(configuration);
+        
+        // configuracaoDAO.deleteForId(4);
+        
+        // for (Configuration i : configuracaoDAO.read()) {
+        //     System.out.print(i.getIdConfig()+" | "+i.isNotification()+" | "+i.isSound()+" | "+i.isShowProfilePhoto()+" | "+i.isShowdescription()+" | "+i.isShowNumber()+" | "+i.isShowEmail()+" | "+i.getTheme()+"\n");
+        // }
     }
 }
--- src/main/java/dao/ConfiguracaoDAO.java
+++ src/main/java/dao/ConfiguracaoDAO.java
@@ -7,8 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ConfiguracaoDAO {
@@ -42,87 +42,85 @@ public class ConfiguracaoDAO {
         return reponse;
     }
 
-    // public List<User> read() {
-    //     List users = new ArrayList();
-    //     connection = MyConnection.getConnection();
-    //     String querySelectUser = "select * from user";
+    public List<Configuration> read() {
+        List configurations = new ArrayList();
+        connection = MyConnection.getConnection();
+        String querySelectUser = "select * from configuration";
 
-    //     try {
-    //         statement = connection.prepareStatement(querySelectUser);
-    //         resultSet = statement.executeQuery();
-    //         while (resultSet.next()) {
-    //             User user = new User();
-    //             user.setIdUser(resultSet.getInt("idUser"));
-    //             user.setName(resultSet.getString("name"));
-    //             user.setSurname(resultSet.getString("surname"));
-    //             user.setEmail(resultSet.getString("email"));
-    //             user.setPhone(resultSet.getString("phone"));
-    //             user.setPassword(resultSet.getString("password"));
-    //             user.setAcceptTerms(resultSet.getBoolean("acceptTerms"));
-    //             user.setDescription(resultSet.getString("description"));
-    //             user.setProfilePhoto(resultSet.getString("profilePhoto"));
-    //             user.setAddress(resultSet.getInt("address"));
-    //             user.setConfiguration(resultSet.getInt("configuration"));
-    //             users.add(user);
-    //         }
+        try {
+            statement = connection.prepareStatement(querySelectUser);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Configuration configuration = new Configuration();
+                configuration.setIdConfig(resultSet.getInt("idConfig"));
+                configuration.setNotification(resultSet.getBoolean("notification"));
+                configuration.setSound(resultSet.getBoolean("sound"));
+                configuration.setShowProfilePhoto(resultSet.getBoolean("showProfilePhoto"));
+                configuration.setShowdescription(resultSet.getBoolean("showdescription"));
+                configuration.setShowNumber(resultSet.getBoolean("showNumber"));
+                configuration.setShowEmail(resultSet.getBoolean("showEmail"));
+                configuration.setTheme(resultSet.getString("theme"));
 
-    //     } catch (SQLException e) {
-    //         System.out.println("Opss... Erro ao selecionar Alunos!..." + e);
-    //     } finally {
-    //         MyConnection.closeConnection(connection, statement, resultSet);
-    //     }
+                configurations.add(configuration);
+            }
 
-    //     return users;
-    // }
+        } catch (SQLException e) {
+            System.out.println("Opss... Erro ao selecionar Alunos!..." + e);
+        } finally {
+            MyConnection.closeConnection(connection, statement, resultSet);
+        }
 
-    // public boolean update(User user) {
-    //     boolean reponse = false;
-    //     connection = MyConnection.getConnection();
-    //     String queryUpdateUser = "update user set name=?,surname=?,email=?,phone=?,password=?,acceptTerms=?,description=?,profilePhoto=?,address=?,configuration=? where idUser=?;";
+        return configurations;
+    }
 
-    //     try {
-    //         statement = connection.prepareStatement(queryUpdateUser);
-    //         statement.setString(1, user.getName());
-    //         statement.setString(2, user.getSurname());
-    //         statement.setString(3, user.getEmail());
-    //         statement.setString(4, user.getPhone());
-    //         statement.setString(5, user.getPassword());
-    //         statement.setBoolean(6, user.isAcceptTerms());
-    //         statement.setString(7, user.getDescription());
-    //         statement.setString(8, user.getProfilePhoto());
-    //         statement.setInt(9, user.getAddress());
-    //         statement.setInt(10, user.getConfiguration());
-    //         statement.setInt(11, user.getIdUser());
-    //         statement.executeUpdate();
-    //         reponse = true;
+    public boolean update(Configuration configuration) {
+        boolean reponse = false;
+        connection = MyConnection.getConnection();
+        String queryUpdateConfiguration = "update configuration set notification=?, sound=?, showProfilePhoto=?, showdescription=?, showNumber=?, showEmail=?, theme=? where idConfig=?;";
 
-    //     } catch (SQLException e) {
-    //         System.out.println("Opss.. Erro ao atualizar Aluno" + e);
-    //     } finally {
-    //         MyConnection.closeConnection(connection, statement);
-    //     }
+        try {
+            statement = connection.prepareStatement(queryUpdateConfiguration);
+            statement.setBoolean(1,configuration.isNotification());
+            statement.setBoolean(2, configuration.isSound());
+            statement.setBoolean(3, configuration.isShowProfilePhoto());
+            statement.setBoolean(4, configuration.isShowdescription());
+            statement.setBoolean(5, configuration.isShowNumber());
+            statement.setBoolean(6, configuration.isShowEmail());
+            statement.setString(7, configuration.getTheme());
+            statement.setInt(8, configuration.getIdConfig());
 
-    //     return reponse;
-    // }
 
-    // public boolean deleteForId(int id) {
-    //     boolean response = false;
-    //     connection = MyConnection.getConnection();
-    //     String queryDeleteUser = "delete from user where idUser=?";
 
-    //     try {
-    //         statement = connection.prepareStatement(queryDeleteUser);
-    //         statement.setInt(1, id);
-    //         statement.executeUpdate();
-    //         response = true;
+            statement.executeUpdate();
+            reponse = true;
 
-    //     } catch (SQLException e) {
-    //         System.out.println("Opss... Erro ao tentar excluir usuário" + e);
-    //     } finally {
-    //         MyConnection.closeConnection(connection, statement);
-    //     }
+        } catch (SQLException e) {
+            System.out.println("Opss.. Erro ao atualizar Aluno" + e);
+        } finally {
+            MyConnection.closeConnection(connection, statement);
+        }
 
-    //     return response;
-    // }
+        return reponse;
+    }
+
+    public boolean deleteForId(int id) {
+        boolean response = false;
+        connection = MyConnection.getConnection();
+        String queryDeleteConfig = "delete from configuration where idConfig=?";
+
+        try {
+            statement = connection.prepareStatement(queryDeleteConfig);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            response = true;
+
+        } catch (SQLException e) {
+            System.out.println("Opss... Erro ao tentar excluir usuário" + e);
+        } finally {
+            MyConnection.closeConnection(connection, statement);
+        }
+
+        return response;
+    }
 
 }
