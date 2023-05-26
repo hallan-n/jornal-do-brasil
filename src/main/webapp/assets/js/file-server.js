36c7d4caf39adc941cf769e476adea9ccb1f85b4
hallan-n <hallanneves01@gmail.com>
2023-05-26 02:05:01 -0300

    Add file-server

--- src/main/webapp/assets/js/file-server.js
+++ src/main/webapp/assets/js/file-server.js
@@ -1,24 +1,15 @@
 const fs = require('fs')
 
-
 const readFile = (path) => {
     fs.readFile(path, 'utf-8', (error, data) => {
-        if (error) {
-            console.log('erro de leitura: ' + error.message)
-        }
-        else {
-            console.log(data)
-        }
-
+        if (error) console.log('erro de leitura: ' + error.message) 
+        else console.log(data)
     })
 }
 writeFile = (path, content) => {
     fs.writeFile(path, content, (error) => {
-        if (error) {
-            console.error('erro de escrita' + error.message)
-        } else {
-            console.log('escreve com sucesso em ' + path)
-        }
+        if (error)console.error('erro de escrita' + error.message) 
+        else console.log('escreve com sucesso em ' + path) 
     })
 }
 
