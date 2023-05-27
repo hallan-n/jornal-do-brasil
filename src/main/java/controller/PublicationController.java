03b9923d68c09173e60f40fa490a7b9dcf72af27
hallan-n <hallanneves01@gmail.com>
2023-05-27 07:03:01 -0300

    Add controller

--- src/main/java/controller/PublicationController.java
+++ src/main/java/controller/PublicationController.java
@@ -1,19 +1,108 @@
 package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import dao.PublicationDAO;
 import model.Publication;
 import services.FileServer;
 
-public class PublicationController {
-    
+public class PublicationController extends HttpServlet{
+
+    private String acao;
+    private String abrir;
+    private final String cadastrar = "cadastrar_aluno.jsp";
+    private final String editar = "editar_aluno.jsp";
+    private final String listar = "listar_alunos.jsp";
+    private final String sucesso = "sucesso.jsp";
+    private final String erro = "erro.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        }
+    }
+
+    // @Override
+    // protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    //         throws ServletException, IOException {
+
+    //     acao = request.getParameter("acao");
+    //     AlunoDAO dao = new AlunoDAO();
+
+    //     if (acao.equals("listar")) {
+    //         abrir = listar;
+    //         request.setAttribute("alunos", dao.read());
+    //     } else if (acao.equals("editar")) {
+    //         int idAluno = Integer.parseInt(request.getParameter("id"));
+    //         request.setAttribute("aluno", dao.findId(idAluno));
+    //         abrir = editar;
+    //     }
+    //     RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
+    //     visualizar.forward(request, response);
+    // }
+
+    // @Override
+    // protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        //     throws ServletException, IOException {
+
+        // acao = request.getParameter("acao");
+        // if (acao.equals("cadastrar")) {
+        //     Aluno aluno = new Aluno();
+        //     aluno.setNome(request.getParameter("txtNome"));
+        //     aluno.setEmail(request.getParameter("txtEmail"));
+        //     aluno.setIdade(Integer.parseInt(request.getParameter("txtIdade")));
+        //     aluno.setTelefone(request.getParameter("txtTelefone"));
+        //     AlunoDAO dao = new AlunoDAO();
+
+        //     if (dao.create(aluno)) {
+        //         abrir = sucesso;
+        //         request.setAttribute("msg", "Uhull... Aluno cadastrado com sucesso!");
+        //     } else {
+        //         abrir = erro;
+        //         request.setAttribute("msg", "Ops... Erro ao cadastrar Aluno!");
+        //     }
+        // } else if (acao.equals("editar")) {
+
+        //     Aluno alu = new Aluno();
+
+        //     alu.setIdAluno(Integer.parseInt(request.getParameter("txtId")));
+        //     alu.setNome(request.getParameter("txtNome"));
+        //     alu.setEmail(request.getParameter("txtEmail"));
+        //     alu.setIdade(Integer.parseInt(request.getParameter("txtIdade")));
+        //     alu.setTelefone(request.getParameter("txtTelefone"));
+
+        //     AlunoDAO dao = new AlunoDAO();
+
+        //     if (dao.update(alu)) {
+        //         abrir = listar;
+        //         request.setAttribute("alunos", dao.read());
+        //     } else {
+        //         abrir = erro;
+        //         request.setAttribute("msg", "Ops... Erro ao atualizar Aluno!");
+
+        //     }
+
+        //     RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
+        //     visualizar.forward(request, response);
+
+        // }
+    // }
+
     public static void main(String[] args) {
         FileServer fileServer = new FileServer();
-        fileServer.setPath("\\src\\main\\java\\storage\\");
+        fileServer.setPath("/src/main/java/storage/");
         fileServer.setFileName("Teste");
         fileServer.setExtension(".html");
         fileServer.writeFile("teste");
-
 
     }
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -14,77 +14,98 @@ import dao.UserDAO;
 import model.User;
 
 public class UserController extends HttpServlet {
-
-    private String action;
-    private String abrir;
+    // Variaveis globais
+    private String acao, abrir;
     private final String cadastrar = "cadastrar_aluno.jsp";
     private final String editar = "editar_aluno.jsp";
     private final String listar = "listar_alunos.jsp";
     private final String sucesso = "sucesso.jsp";
     private final String erro = "erro.jsp";
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        action = request.getParameter("action");
-        UserDAO userDAO = new UserDAO();
-
-        if (action.equals("listar")) {
-            abrir = listar;
-            request.setAttribute("alunos", userDAO.read());
-
-        } else if (action.equals("editar")) {
-            int idAluno = Integer.parseInt(request.getParameter("id"));
-            // request.setAttribute("aluno", action.findId(idAluno));
-            abrir = editar;
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
         }
-
-        RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
-
-        visualizar.forward(request, response);
-
     }
 
+    // @Override
+    // protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    //         throws ServletException, IOException {
+
+    //     acao = request.getParameter("acao");
+    //     UserDAO userDAO = new UserDAO();
+
+    //     if (acao.equals("listar")) {
+    //         abrir = listar;
+    //         request.setAttribute("alunos", userDAO.read());
+    //     } else if (acao.equals("editar")) {
+    //         int idAluno = Integer.parseInt(request.getParameter("id"));
+    //         request.setAttribute("aluno", userDAO.findId(idAluno));
+    //         abrir = editar;
+    //     }
+    //     RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
+    //     visualizar.forward(request, response);
+    // }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        action = request.getParameter("action");
-        Boolean acceptTerms = false;
-        UserDAO userDAO = new UserDAO();
-
-        if (action.equals("cadastrar")) {
+        boolean acceptTerms = false;
+        acao = request.getParameter("acao");
+        if (acao.equals("cadastrar")) {
             User user = new User();
-            user.setName(request.getParameter("txtName"));
-            user.setSurname(request.getParameter("txtSurname"));
-            user.setEmail(request.getParameter("txtEmail"));
-            user.setPhone(request.getParameter("txtPhone"));
-            user.setPassword(request.getParameter("txtPassword"));
-            if (request.getParameter("txtAcceptTerms") == "on") {
+            user.setName(request.getParameter("txtNome"));
+            user.setSurname(request.getParameter("txtNome"));
+            user.setEmail(request.getParameter("txtNome"));
+            user.setPhone(request.getParameter("txtNome"));
+            user.setPassword(request.getParameter("txtNome"));
+            if(request.getParameter("txtNome")=="on"){
                 acceptTerms = true;
             }
             user.setAcceptTerms(acceptTerms);
+            
 
-            if (userDAO.create(user)) {
+
+
+
+
+
+
+
+            aluno.setNome(request.getParameter("txtNome"));
+            aluno.setEmail(request.getParameter("txtEmail"));
+            aluno.setIdade(Integer.parseInt(request.getParameter("txtIdade")));
+            aluno.setTelefone(request.getParameter("txtTelefone"));
+
+            AlunoDAO userDAO = new AlunoDAO();
+
+            if (userDAO.create(aluno)) {
                 abrir = sucesso;
                 request.setAttribute("msg", "Uhull... Aluno cadastrado com sucesso!");
             } else {
                 abrir = erro;
                 request.setAttribute("msg", "Ops... Erro ao cadastrar Aluno!");
             }
-        } else if (action.equals("editar")) {
-            User user = new User();
-            user.setName(request.getParameter("txtName"));
-            user.setSurname(request.getParameter("txtSurname"));
-            user.setEmail(request.getParameter("txtEmail"));
-            user.setPhone(request.getParameter("txtPhone"));
-            user.setPassword(request.getParameter("txtPassword"));
+        } else if (acao.equals("editar")) {
 
-            if (userDAO.update(user)) {
+            Aluno alu = new Aluno();
+
+            alu.setIdAluno(Integer.parseInt(request.getParameter("txtId")));
+            alu.setNome(request.getParameter("txtNome"));
+            alu.setEmail(request.getParameter("txtEmail"));
+            alu.setIdade(Integer.parseInt(request.getParameter("txtIdade")));
+            alu.setTelefone(request.getParameter("txtTelefone"));
+
+            AlunoDAO userDAO = new AlunoDAO();
+
+            if (userDAO.update(alu)) {
                 abrir = listar;
                 request.setAttribute("alunos", userDAO.read());
             } else {
                 abrir = erro;
                 request.setAttribute("msg", "Ops... Erro ao atualizar Aluno!");
+
             }
 
             RequestDispatcher visualizar = request.getRequestDispatcher(abrir);
