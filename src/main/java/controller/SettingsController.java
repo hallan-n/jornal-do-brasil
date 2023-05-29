6c2f5a23a60eef0f4b8190944421e8a0df27d26f
hallan-n <hallanneves01@gmail.com>
2023-05-29 19:27:10 -0300

    Add file-server.js

--- src/main/java/controller/SettingsController.java
+++ src/main/java/controller/SettingsController.java
@@ -1,9 +1,29 @@
 package controller;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import dao.SettingsDAO;
 import model.Configuration;
 
-public class SettingsController {
-    public void crud(){
-        SettingsDAO configDAO = new SettingsDAO();
-    }
+@WebServlet("/teste2")
+public class SettingsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hello from Servlet");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
+
